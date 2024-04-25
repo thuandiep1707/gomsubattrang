bc902b9143d17705d582499e6847394c6da5173c
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-04-25 17:49:54 +0700

    update

---
+++ vendors/data.js
@@ -0,0 +1,9 @@
+import logo from '../assets/img/logo.png'
+import search from '../assets/img/search-icon.png'
+import user from '../assets/img/user-icon.png'
+import heart from '../assets/img/heart-icon.png'
+import cart from '../assets/img/cart-icon.png'
+// import search from '../assets/img/'
+
+
+export {logo, search, user, heart, cart}
--- vendors/services.js
+++ vendors/services.js
@@ -1,7 +1,9 @@
-const getCollection = () =>{
-    return {
-        "an": ["tô","thố"]
-    }
+async function getCollection (){
+    let data = await fetch('https://660a67010f324a9a2884edd1.mockapi.io/gomsuColection')
+                .then(res => res.json())
+                .then(data => data[0])
+    delete data['id']
+    return data
 }
 
 export { getCollection }
